157d310fd86c3346fa389c6c8b6fcb02c8d3f77b
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-16 17:29:27 +0900

    [FEAT] refactoring 어렵다...

--- src/main/java/com/videorental/Customer.java
+++ src/main/java/com/videorental/Customer.java
@@ -27,42 +27,57 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 
 		while ( iterator.hasNext() ) {
-			double thisAmount = 0;
 			Rental each = (Rental) iterator.next();
-			// determine amounts for each line
 
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-				
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-				
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
-
-			// add frequent renter points
+			double thisAmount = getRentalAmount(each);
 			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
-			// show figures
-			result += "\t" +  String.valueOf(thisAmount) + "(" + each.getMovie().getTitle() + ")" + "\n";
+			if (isNewRealseBonus(each)) frequentRenterPoints++;
+
+			result += "\t"
+					+  String.valueOf(thisAmount)
+					+ "(" + each.getMovie().getTitle()
+					+ ")" + "\n";
 
 			totalAmount += thisAmount;
 		}
 
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter pointers";
+		result += "Amount owed is "
+				+ String.valueOf(totalAmount)
+				+ "\n"
+				+ "You earned "
+				+ String.valueOf(frequentRenterPoints)
+				+ " frequent renter pointers";
 
 		return result;
 	}
+
+	private double getRentalAmount(Rental rental){
+
+		double thisAmount = 0;
+
+		switch (rental.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (rental.getDaysRented() > 2)
+					thisAmount += (rental.getDaysRented() - 2) * 1.5;
+				break;
+
+			case Movie.NEW_RELEASE:
+				thisAmount += rental.getDaysRented() * 3;
+				break;
+
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (rental.getDaysRented() > 3)
+					thisAmount += (rental.getDaysRented() - 3) * 1.5;
+				break;
+
+		}
+		return thisAmount;
+	}
+
+	private boolean isNewRealseBonus(Rental rental){
+		return ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1 );
+	}
+
 }
--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -1,5 +1,156 @@
 package com.videorental;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerTest {
+
+    public static final String NAME = "NAME_NOT_IMPORTANT";
+    public static final String TITLE = "TITLE_NOT_IMPORTANT";
+
+    Customer customer = new Customer(NAME);
+
+    private static Rental createRentalFor(int daysRented, int priceCode) {
+        Movie movie = new Movie(TITLE, priceCode);
+        return new Rental(movie, daysRented);
+    }
+
+    @Test
+    void statement() {
+        customer.addRental(createRentalFor(4, Movie.REGULAR));
+
+        String result = customer.statement();
+        System.out.println(result);
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t5.0(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 5.0\n" +
+                "You earned 1 frequent renter pointers";
+
+
+        assertEquals(expected, result);
+    }
+
+
+
+    @Test
+    void statement2() {
+
+        customer.addRental(createRentalFor(3, Movie.CHILDRENS));
+        customer.addRental(createRentalFor(2, Movie.REGULAR));
+        customer.addRental(createRentalFor(6, Movie.NEW_RELEASE));
+        customer.addRental(createRentalFor(2, Movie.REGULAR));
+        customer.addRental(createRentalFor(4, Movie.CHILDRENS));
+        customer.addRental(createRentalFor(3, Movie.NEW_RELEASE));
+
+        String result = customer.statement();
+        System.out.println(result);
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t1.5(TITLE_NOT_IMPORTANT)\n" +
+                "\t2.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t18.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t2.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t9.0(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 35.5\n" +
+                "You earned 8 frequent renter pointers";
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void noReantal(){
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void noRegular2(){
+        customer.addRental(createRentalFor(2, Movie.REGULAR));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t2.0(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void noRegular3(){
+        customer.addRental(createRentalFor(3, Movie.REGULAR));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t3.5(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void newRelease(){
+        customer.addRental(createRentalFor(1, Movie.NEW_RELEASE));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void newRelease2(){
+        customer.addRental(createRentalFor(2, Movie.NEW_RELEASE));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t6.0(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 2 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void children4(){
+        customer.addRental(createRentalFor(4, Movie.CHILDRENS));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void children3(){
+        customer.addRental(createRentalFor(3, Movie.CHILDRENS));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t1.5(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void setTest(){
+        Movie movie = new Movie(TITLE, Movie.REGULAR);
+        movie.setPriceCode(Movie.CHILDRENS);
+        customer.addRental(new Rental(movie, 3));
+        String result = customer.statement();
+        String expected = "Rental Record for NAME_NOT_IMPORTANT\n" +
+                "\t1.5(TITLE_NOT_IMPORTANT)\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter pointers";
+        System.out.println(result);
+        assertEquals(expected, result);
+    }
 
 }
